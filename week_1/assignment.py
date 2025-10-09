0344d491657a9e8b4fe233632493213463383631
Clinton Anene <clintonanene591gmail.com>
2025-10-09 14:55:26 -0700

    first commit

---
+++ week_1/assignment.py
@@ -0,0 +1,112 @@
+# PYTHON BEGINNER ASSIGNMENT ======================
+
+# 1. Write:
+#    - One single-line comment about Python (use #)
+#    - One multi-line comment about Python (use ''' ''' or """ """)
+
+
+# Write Answer 1 below
+# Python can be used for Data Analysis
+"""
+Python is a programming tool
+python is used in web development
+python is used in data science
+"""
+
+
+
+# 2. Make 5 variables with these types:
+#    - A string (text)
+#    - An integer (whole number)
+#    - A float (number with decimal)
+#    - A boolean (True or False)
+#    - Another string using snake_case (example: my_name)
+
+# Write Answer 2 below
+my_string ="Clinton"
+my_integers = 15
+my_float = 46.7
+my_bool = True
+snake_case = "my_house"
+
+
+
+# 3. Use x = 10 and y = 3
+#    Print the answer AND the type for:
+#    - x + y
+#    - x - y
+#    - x * y
+#    - x / y
+#    - x // y
+
+
+# Write Answer 3 below
+x = 10 
+y =3
+print(x + y)
+print(x - y)
+print(x * y)
+print(x/y)
+print(x//y)
+
+
+# 4. Join (concatenate) two strings to make a sentence, then print it.
+
+# Write Answer 4 below
+print("My name is " + "Anene Clinton")
+# 5. Print the word "Python" three times using * (multiplication).
+
+# Write Answer 5 below
+print( "python" * 3)
+
+# 6. Change (cast) these and print the value and type:
+#    - 45.89 ➝ integer
+#    - 78 ➝ string
+#    - "1234" ➝ float
+
+# Write Answer 6 below
+num_1 = int(45.89)
+print(num_1, type(num_1))
+num_2 = str(78)
+print(num_2, type(num_2))
+num_3 = float("1234")
+print(num_3, type(num_3))
+
+# 7. Make a variable called country.
+#    Change it 3 times to different countries.
+#    Print the final one.
+
+# Write Answer 7 below
+country = "Nigeria"
+country = "America"
+country = "Japan"
+print(country)
+
+# 8. Make a sentence using + to join strings.
+#    Example: "My name is " + your_name + " and I like " + your_hobby
+
+# Write Answer 8 below
+your_name = "Clinton"
+your_hobby = "Basketball"
+print("My name is " + your_name + " and I like " + your_hobby)
+
+# 9. What will this show?
+#    result = "5" + "7"
+#    print(result, type(result))
+#    Write your answer here as a comment:
+
+# Write Answer 9 below
+#12, type=integer
+
+# 10. Create two variables with numbers.
+#     Add them together, but before printing, convert the result to a string.
+#     Then print it inside a sentence like: "The answer is 10"
+
+# Write Answer 10 below
+x=60
+y =34
+result = x + y
+type(result)
+print("The answer is " + str(result))
+
+
