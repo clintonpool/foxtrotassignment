a38b322b8e033409eb7028f919776efdeb1d0282
Clinton Anene <clintonanene591gmail.com>
2025-10-15 13:55:09 -0700

    Added classwork3.py and initialized project

--- classwork3.py
+++ classwork3.py
@@ -1,3 +1,10 @@
+#1. Create a list called expenses. It's going to be a list of dictionary in this format e.g [{"expense": "Transportation", "price": 400}].
+#2. create inputs: expense, price.
+#3. Create a dictionary with the keys and values of expense and price.
+#4. Write an if else statement. If price is greater than (put your price), update the dictionary with the key "is_over_the_budget", set True and append to expenses.
+  # Else add the same type of key "is_over_the_budget", set to False and append to expenses.
+#5. Debug at the end to check if your code is working as expected. let us start over answering exactly what is expected 
+
 expenses = []
 expense = input("Enter the expense name: ")
 price = float(input("Enter the expense amount: "))
